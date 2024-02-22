b2def5b41199615f2fd1ecae29484401dafe0d06
ediezindell <ediezindell@gmail.com>
2024-02-23 07:02:19 +0900

    add mklink.sh

---
+++ mklink.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+files=.*
+for file in ${files}; do
+  filepath="${PWD}/${file}"
+  homefile="${HOME}/${file}"
+
+  test $file == "." && continue
+  test $file == ".." && continue
+  test $file == ".config" && continue
+  test $file == ".git" && continue
+  test $file == ".gitmodules" && continue
+
+  if [ ! -a "${homefile}" -a ! -h "${homefile}" ]; then
+    echo "${file} not exist, make symbolic link to ${homefile}"
+    ln -s "${filepath}" "${homefile}"
+  else
+    echo "${file} exist, skip"
+  fi
+done
+
+configfiles=.config/*
+for file in ${configfiles}; do
+  filepath="${PWD}/${file}"
+  homefile="${HOME}/${file}"
+
+  test $file == "." && continue
+  test $file == ".." && continue
+
+  if [ ! -a "${homefile}" -a ! -h "${homefile}" ]; then
+    echo "${file} not exist, make symbolic link to ${homefile}"
+    ln -s "${filepath}" "${homefile}"
+  else
+    echo "${file} exist, skip"
+  fi
+done
