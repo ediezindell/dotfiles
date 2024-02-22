de499dea0efc64da039d55fb31efa558da0d03b0
ediezindell <ediezindell@gmail.com>
2024-02-23 07:22:09 +0900

    add .zshrc

---
+++ .zsh.d/alias.zsh
@@ -0,0 +1,24 @@
+alias ll="ls -l"
+alias la="ls -la"
+
+alias rm="rm -i"
+alias cp="cp -i"
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias d="docker"
+alias dc="docker compose"
+alias dce="docker compose exec"
+alias dcd="docker compose down"
+alias dcud="docker compose up -d"
+alias dcudb="docker compose up -d --build"
+
+alias glo="git log --oneline"
+alias gcm="git checkout master"
+alias gc="git checkout"
+
+alias vi="nvim"
+alias vim="nvim"
+
+alias cb="system_profiler SPPowerDataType | grep Wattage"
+
