10df11d0d179340b67f3d59b827d676ede0de834
ediezindell <ediezindell@gmail.com>
2024-02-25 09:38:07 +0900

    update gitsigns settings

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,5 +2,11 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = "VimEnter",
-  config = true,
+  config = function()
+    require('gitsigns').setup({
+      numhl = true,
+      -- linehl = true,
+      -- word_diff = true,
+    })
+  end,
 }
