c687594d8d55ffae4a3627f7b99675f88584eba8
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-02-29 15:35:56 +0900

    update lspsaga setting

--- .config/nvim/lua/plugins/lspsaga.lua
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -1,7 +1,8 @@
 return {
   "kkharji/lspsaga.nvim",
-  config = function()
-    require("lspsaga").setup({
-    })
-  end,
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = true,
 }
