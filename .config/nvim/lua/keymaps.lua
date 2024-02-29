9aef7a34e9dcc987c51fd2bca3b836d11a8e4874
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-02-29 15:42:15 +0900

    update keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -9,11 +9,11 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Esc2回でハイライト解除
-keymap("n", "<Esc><Esc>", ":<C-u>set nohlsearch<Return>", opts)
+keymap("n", "<Esc><Esc>", ":<C-u>set nohlsearch<CR>", opts)
 
 -- split view
-keymap("n", "ss", ":split<Return><C-w>w", opts) -- 上下
-keymap("n", "sv", ":vsplit<Return><C-w>w", opts) -- 左右
+keymap("n", "ss", ":split<CR>", opts)  -- 上下
+keymap("n", "sv", ":vsplit<CR>", opts) -- 左右
 
 -- donot yank
 keymap("n", "x", '"_x', opts)
@@ -21,15 +21,16 @@ keymap("n", "X", '"_X', opts)
 keymap("n", "s", '"_s', opts)
 
 -- telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 -- nvim-tree
-keymap('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
-keymap('n', '<leader>t', ':bo term<CR>', term_opts)
+-- terminal
+keymap("n", "<leader>t", ":term<CR>", term_opts)
 
-keymap('n', '<leader>gd', ':DiffviewOpen<CR>', opts)
+keymap("n", "<leader>gd", ":DiffviewOpen<CR>", opts)
