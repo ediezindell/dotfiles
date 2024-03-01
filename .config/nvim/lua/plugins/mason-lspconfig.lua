1253ff42f72ad36742386c109e6630f2c3abd631
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-03-01 12:47:51 +0900

    format

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -56,7 +56,7 @@ return {
     })
 
     vim.lsp.handlers["textDocument/publishDiagnostics"] =
-        vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
+      vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
     vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
       border = "single", -- "shadow" , "none", "rounded"
       -- border = border
