692a22a4245908b154153fc1570540612f1590e5
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-03-05 10:22:02 +0900

    update telescope settings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,33 @@ return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.5",
   -- event = "VimEnter",
-  dependencies = {
+  requires = {
+    "nvim-tree/nvim-web-devicons",
     "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
   },
+  config = function()
+    local telescope = require("telescope")
+
+    telescope.setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-h>"] = "which_key",
+          },
+        },
+        winblend = 20,
+      },
+    })
+    -- telescope.load_extension("fzf")
+
+    local builtin = require("telescope.builtin")
+
+    vim.keymap.set("n", "<leader>ff", builtin.find_files)
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+    vim.keymap.set("n", "<leader>fb", builtin.buffers)
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+
+
+  end,
 }
