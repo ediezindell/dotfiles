0bd311674e2270f14ced902b8bbed8826e950c53
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-03-05 15:04:59 +0900

    remove italic styles

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -1,1 +1,12 @@
 vim.cmd([[colorscheme monokai]])
+
+local italicTypes = {
+  "Function",
+  "Keyword",
+  "Include",
+  "SpecialComment",
+  "Comment",
+}
+for _, type in pairs(italicTypes) do
+  vim.cmd("hi " .. type .. " gui=NONE")
+end
