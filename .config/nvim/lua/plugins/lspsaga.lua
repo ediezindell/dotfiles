ff05a3d02eb561d3d0af15166130e39879a72baf
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-03-05 15:04:41 +0900

    update lsp ui settings

--- .config/nvim/lua/plugins/lspsaga.lua
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -12,5 +12,23 @@ return {
       hint_sign = "󰌵",
       infor_sign = "",
     })
+
+    local floatConfig = {
+      focusable = true,
+      style = "minimal",
+      border = "rounded",
+    }
+    vim.diagnostic.config({
+      virtual_text = {
+        spacing = 4,
+        -- prefix = "●",
+      },
+      underline = true,
+      update_in_insert = false,
+      severity_sort = true,
+      float = floatConfig,
+    })
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, floatConfig)
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, floatConfig)
   end,
 }
--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -54,13 +54,5 @@ return {
         vim.keymap.set("n", "g[", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
       end,
     })
-
-    vim.lsp.handlers["textDocument/publishDiagnostics"] =
-      vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-      border = "single", -- "shadow" , "none", "rounded"
-      -- border = border
-      -- width = 100,
-    })
   end,
 }
