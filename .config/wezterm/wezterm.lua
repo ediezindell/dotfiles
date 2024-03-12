f29f15b8b8a7f5cdbe722d80ed1a24a2a25f1d6a
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-03-12 12:05:07 +0900

    update wezterm settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,22 +1,30 @@
 local wezterm = require("wezterm")
--- local keybind = require("keybinds")
+local keybind = require("keybinds")
+
+local fonts = {
+  "Moralerspace Argon HWNF",
+  "Moralerspace Neon HWNF",
+  "FirgeNerd",
+  "FirgeNerd Console",
+  "HackGen Console NF",
+  "Firge",
+}
 
 local config = {
   -- appearance
   color_scheme = "nord",
-  window_background_opacity = 0.90,
-  -- font = wezterm.font("FirgeNerd Console", {
-  font = wezterm.font("HackGen Console NF", {
-    -- font = wezterm.font("Firge", {
+  window_background_opacity = 0.9,
+  font = wezterm.font(fonts[0], {
     weight = "Medium",
     stretch = "Normal",
     style = "Normal",
   }),
   font_size = 16,
+
   -- keybinds
-  -- disable_default_key_bindings = true,
-  -- keys = keybind.keys,
-  -- key_tables = keybind.key_tables,
+  disable_default_key_bindings = true,
+  keys = keybind.keys,
+  key_tables = keybind.key_tables,
 }
 
 return config
