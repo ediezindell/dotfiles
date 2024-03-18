73a5504cc9882a710719f5a6c02faa6da28a18a3
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-03-18 09:11:53 +0900

    update zsh autoload

--- .zsh.d/autoload.zsh
+++ .zsh.d/autoload.zsh
@@ -1,3 +1,15 @@
+# starship
 eval "$(starship init zsh)"
 . "$HOME/.asdf/asdf.sh"
+
+# volta
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
+
+# pnpm
+export PNPM_HOME="$HOME/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
 
