ac8eb74d7a7f33931549cda384a5a09567ba7bb8
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-03-27 14:37:35 +0900

    add flash

---
+++ .config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,24 @@
+-- Mでジャンプ
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    {
+      "M",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "<c-c>",
+      mode = { "c" },
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle Flash Search",
+    },
+  },
+}
