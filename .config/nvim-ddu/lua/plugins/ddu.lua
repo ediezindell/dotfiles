cac22871176ad8e88cae83518354a7224ed9064c
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-04-02 06:56:26 +0900

    add comments

--- .config/nvim-ddu/lua/plugins/ddu.lua
+++ .config/nvim-ddu/lua/plugins/ddu.lua
@@ -7,17 +7,18 @@ return {
     "ryota2357/ddu-column-icon_filename",
     "Shougo/ddu-commands.vim",
     ------------------------------
-    -- | filter                   |
+    -- | filter                  |
     ------------------------------
     "uga-rosa/ddu-filter-converter_devicon", -- icon
+    "ryota2357/ddu-column-icon_filename",
     "yuki-yano/ddu-filter-fzf",
     ------------------------------
-    -- | kinds                    |
+    -- | kinds                   |
     ------------------------------
     "Shougo/ddu-kind-file",
     "Shougo/ddu-kind-word",
     ------------------------------
-    -- | source                   |
+    -- | source                  |
     ------------------------------
     "Shougo/ddu-source-file",
     "Shougo/ddu-source-file_rec",
@@ -26,28 +27,28 @@ return {
     "Shougo/ddu-source-line",
     "Shougo/ddu-source-register",
     ------------------------------
-    -- | ui                       |
+    -- | ui                      |
     ------------------------------
     "Shougo/ddu-ui-ff", -- fuzzy_finder(あいまい検索)
   },
   config = function()
     ------------------------------
-    -- | global                   |
+    -- | global                  |
     ------------------------------
     vim.fn["ddu#custom#patch_global"]({
       ui = "ff",
       uiParams = {
         ff = {
+          floatingBorder = "rounded",        -- フローティングウィンドウのborder設定
           filterFloatingPosition = "bottom",
-          filterSplitDirection = "floating",
-          floatingBorder = "rounded",
+          filterSplitDirection = "floating", -- フィルターをフローティングウィンドウで表示
           previewFloating = true,
           previewFloatingBorder = "rounded",
           previewFloatingTitle = "Preview",
           previewSplit = "horizontal",
-          prompt = "> ",
-          split = "floating",
-          startFilter = true,
+          prompt = "> ",      -- フィルター入力欄の表示
+          split = "floating", -- フローティングウィンドウで表示する
+          startFilter = true, -- 自動的にフィルターも表示する
           startAutoAction = true,
           autoAction = { name = "preview", delay = 0 },
         },
@@ -86,7 +87,7 @@ return {
     })
 
     ------------------------------
-    -- | local                    |
+    -- | local                   |
     ------------------------------
     vim.fn["ddu#custom#patch_local"]("file_rec", {
       sources = {
@@ -126,7 +127,7 @@ return {
     })
 
     ------------------------------
-    -- | autocmd                  |
+    -- | keymap                  |
     ------------------------------
     local opts = { noremap = true, silent = true, buffer = true }
     vim.keymap.set({ "n" }, "<leader>fd", [[<Cmd>call ddu#start(#{name:"file_rec"})<CR>]], opts)
@@ -134,14 +135,19 @@ return {
       pattern = "ddu-ff",
       callback = function()
         vim.keymap.set({ "n" }, "q", [[<Cmd>call ddu#ui#do_action("quit")<CR>]], opts)
-        vim.keymap.set({ "n" }, "<CR>", [[<Cmd>call ddu#ui#do_action("itemAction")<CR>]], opts)
+        vim.keymap.set(
+          { "n" },
+          "<CR>",
+          [[<Cmd>call ddu#ui#do_action("closePreviewWindow")<CR><Cmd>call ddu#ui#do_action("itemAction")<CR>]],
+          opts
+        )
         vim.keymap.set({ "n" }, "i", [[<Cmd>call ddu#ui#do_action("openFilterWindow")<CR>]], opts)
       end,
     })
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "ddu-ff-filter",
       callback = function()
-        vim.keymap.set({ "n", "i" }, "<CR>", [[<Esc><Cmd>close<CR>]], opts)
+        vim.keymap.set({ "n", "i" }, "<CR>", [[<Esc><Cmd>call ddu#ui#do_action("leaveFilterWindow")<CR>]], opts)
       end,
     })
   end,
