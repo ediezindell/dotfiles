a991a0bfaca11c3f6732e5cbf52dbdda77789ebc
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-07-23 10:28:19 +0900

    fix hlchunk settings

--- .config/nvim/lua/plugins/hlchunk.lua
+++ .config/nvim/lua/plugins/hlchunk.lua
@@ -1,6 +1,17 @@
 -- インデントを可視化する
 return {
   "shellRaining/hlchunk.nvim",
-  event = { "UIEnter" },
-  config = true,
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("hlchunk").setup({
+      chunk = {
+        enable = true,
+        style = {
+          { fg = "#9e86c8" },
+          { fg = "#f92472" },
+        },
+        delay = 0,
+      },
+    })
+  end,
 }
