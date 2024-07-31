42293e48b81facd03b2c2ca552b4fe277fa7c8d7
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-08-01 08:42:54 +0900

    update: lua_ls settings

--- .config/nvim/lua/lsp/lua.lua
+++ .config/nvim/lua/lsp/lua.lua
@@ -1,10 +1,21 @@
+local rtp = vim.api.nvim_get_runtime_file("", true)
 return {
   settings = {
     Lua = {
+      hint = { enable = true },
+      format = { enable = true },
+      runtime = {
+        version = "LuaJIT",
+        checkThirdParty = true,
+      },
       diagnostics = {
-        globals = {
-          "vim",
-        },
+        globals = { "vim", "wezterm" },
+      },
+      workspace = {
+        library = rtp,
+      },
+      telemetry = {
+        enable = false,
       },
     },
   },
