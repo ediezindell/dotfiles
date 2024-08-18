5cbb334f92c79ef4318ce073afcd259d2f1bb1d5
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-08-19 08:44:41 +0900

    update colorscheme

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -2,17 +2,19 @@ vim.api.nvim_create_autocmd({ "ColorScheme", "LspAttach" }, {
   callback = function()
     -- italicにしないようguiを再設定
     local italicGroup = {
-      "@keyword.function",
-      "@keyword",
-      "Keyword",
-      "@function",
-      "Function",
-      "@include",
-      "Include",
-      "@specialComment",
-      "SpecialComment",
       "@comment",
+      "@function",
+      "@function.macro",
+      "@include",
+      "@keyword",
+      "@keyword.function",
+      "@specialComment",
+      "@text.reference",
       "Comment",
+      "Function",
+      "Include",
+      "Keyword",
+      "SpecialComment",
     }
     for _, group in ipairs(italicGroup) do
       vim.cmd("hi " .. group .. " gui=NONE")
