cf2108e89474dbb7710f826871c0d2bd11b27c87
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-08-19 10:00:54 +0900

    update colorscheme settings

--- .config/nvim/lua/plugins/monokai.lua
+++ .config/nvim/lua/plugins/monokai.lua
@@ -2,7 +2,24 @@
 ---@type LazySpec
 local spec = {
   "tanvirtin/monokai.nvim",
-  event = "VimEnter",
+  event = "LspAttach",
+  config = function()
+    local monokai = require("monokai")
+    local palette = monokai.classic
+    local getFg = function(color)
+      return { fg = color }
+    end
+    monokai.setup({
+      italics = false,
+      custom_hlgroups = {
+        Structure = getFg(palette.green),
+        typescriptImport = getFg(palette.pink),
+        typescriptExport = getFg(palette.aqua),
+        typescriptVariable = getFg(palette.aqua),
+        tsxIntrinsicTagName = getFg(palette.pink),
+      },
+    })
+  end,
 }
 
 return spec
