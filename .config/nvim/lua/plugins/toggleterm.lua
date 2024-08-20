baaf0c557e550f3f8679bfe8da3714d50de3b67e
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-08-20 09:12:41 +0900

    keymapの指定方法を変更

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -10,7 +10,7 @@ local spec = {
 
     -- keymap
     local term_opts = { silent = true }
-    vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>", term_opts)
+    vim.keymap.set("n", "<leader>t", "<Cmd>ToggleTerm<CR>", term_opts)
   end,
   event = "VeryLazy",
 }
