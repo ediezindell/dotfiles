f7583fb12cd7e681136785bd5cef342b0ee4dd7b
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-09-26 10:16:16 +0900

    update: wezterm settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -61,6 +61,34 @@ local config = {
   disable_default_key_bindings = true,
   keys = keybind.keys,
   key_tables = keybind.key_tables,
+  macos_window_background_blur = 20,
+  window_decorations = "RESIZE",
+  hide_tab_bar_if_only_one_tab = true,
+  show_new_tab_button_in_tab_bar = false,
+  show_close_tab_button_in_tabs = false,
+  colors = {
+    tab_bar = {
+      inactive_tab_edge = "none",
+    },
+  },
 }
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local background = "#2E323C"
+  local foreground = "#f8f8f0"
+
+  if tab.is_active then
+    background = "#4D5154"
+  end
+
+  local space = "      "
+  local title = space .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. space
+
+  return {
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+  }
+end)
 
 return config
