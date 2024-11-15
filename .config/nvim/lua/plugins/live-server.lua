891c8a620162436238f6cea22c4a214db1f0edd4
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-11-15 14:54:06 +0900

    remove: nvim plugins

--- .config/nvim/lua/plugins/live-server.lua
+++
@@ -1,9 +0,0 @@
----@type LazySpec
-local spec = {
-  "barrett-ruth/live-server.nvim",
-  build = "pnpm add -g live-server",
-  cmd = { "LiveServerStart", "LiveServerStop" },
-  config = true,
-}
-
-return spec
