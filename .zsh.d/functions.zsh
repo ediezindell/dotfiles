a9d50b39c7417ff8b8f7a1585f03bf1dc464c3f8
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-11-21 09:05:16 +0900

    update shell functions

--- .zsh.d/functions.zsh
+++ .zsh.d/functions.zsh
@@ -1,3 +1,22 @@
+function node_install() {
+  if [ -f .node-version ]; then
+    CWD_NODE_VERSION=$(cat .node-version)
+    echo $CWD_NODE_VERSION
+    # volta install node@$CWD_NODE_VERSION
+  fi
+}
+
+function bm() {
+  if [ ! -d .git ]; then
+    branches=$(git branch --sort=-committerdate | awk '{print $NF}')
+  else
+    touch .git/memo
+    branches=$(git branch --sort=-committerdate | awk '{print $NF}' | xargs -L1 -I{} grep "{}" .git/memo)
+  fi
+  echo $branches | cat -n
+  read branchNumber\?"Enter Branch Number: "
+  git switch $(echo $branches | cut -d, -f1 | awk NR==$branchNumber)
+}
 function b() {
   branches=$(git branch --sort=-committerdate)
   echo $branches | cat -n
@@ -8,7 +27,6 @@ function ba() {
   branches=$(git branch -a --sort=-committerdate)
   echo $branches | cat -n
   read branchNumber\?"Enter Branch Number: "
-  echo ${branches//remote\/origin\//}
   git switch $(echo ${branches//remotes\/origin\//} | cut -c 3- | awk NR==$branchNumber)
 }
 
@@ -26,3 +44,18 @@ function fg() {
     builtin fg "$@"
   fi
 }
+
+function pmc() {
+  if [ -e package-lock.json ]; then
+    echo "npm"
+  elif [ -e yarn.lock ]; then
+    echo "yarn"
+  elif [ -e pnpm-lock.yaml ]; then
+    echo "pnpm"
+  else
+    echo "?"
+  fi
+}
+
+add-zsh-hook chpwd node_install
+
