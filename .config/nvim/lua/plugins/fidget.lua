3656a815260025ad41ced4e8de0c3ae6f0e416a4
ediezindell <ediezindell@gmail.com>
2024-12-08 09:32:10 +0900

    feat: マクロ記録中にfidgetで表示

--- .config/nvim/lua/plugins/fidget.lua
+++ .config/nvim/lua/plugins/fidget.lua
@@ -3,6 +3,30 @@
 local spec = {
   "j-hui/fidget.nvim",
   event = "VeryLazy",
-  opts = {},
+  config = function()
+    local fidget = require("fidget")
+    fidget.setup({})
+
+    -- マクロ記録中表示
+    local ns = vim.api.nvim_create_namespace("extmode")
+    local progress = nil
+    local extmode = vim.schedule_wrap(function()
+      local reg = vim.fn.reg_recording()
+      if reg ~= "" then
+        if progress == nil then
+          progress = fidget.progress.handle.create({
+            lsp_client = { name = string.format("[@%s] Recording Macro", reg) },
+            message = "Recording...",
+          })
+        end
+      else
+        if progress ~= nil then
+          progress:finish()
+          progress = nil
+        end
+      end
+    end)
+    vim.on_key(Debounce(extmode, 100), ns)
+  end,
 }
 return spec
