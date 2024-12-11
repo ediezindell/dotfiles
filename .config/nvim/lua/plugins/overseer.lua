6380b71b1791257606d86be50b3ff963e1b8c3ef
ediezindell <ediezindell@gmail.com>
2024-12-11 19:41:55 +0900

    update: overseer options

--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -2,11 +2,29 @@
 local spec = {
   "stevearc/overseer.nvim",
   event = "VeryLazy",
+  init = function()
+    vim.api.nvim_create_user_command("OverseerRestartLast", function()
+      local overseer = require("overseer")
+      local tasks = overseer.list_tasks({ recent_first = true })
+      if vim.tbl_isempty(tasks) then
+        vim.notify("No tasks found", vim.log.levels.WARN)
+      else
+        overseer.run_action(tasks[1], "restart")
+      end
+    end, {})
+  end,
   keys = {
     { "<space>or", "<Cmd>OverseerRun<CR>", desc = "OverseerRun" },
     { "<space>oo", "<Cmd>OverseerOpen<CR>", desc = "OverseerOpen" },
+    { "<space>ol", "<Cmd>OverseerRestartLast<CR>", desc = "OverseerRestartLast" },
   },
-  opts = {},
+  opts = {
+    -- strategy = {
+    --   "toggleterm",
+    --   use_shell = false,
+    --   close_on_exit = false,
+    -- },
+  },
 }
 
 return spec
