075f5ded3b37a41ef1ad4e5dd804498e21db618e
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-12-18 11:22:14 +0900

    add: lspctl

---
+++ .config/nvim/lua/plugins/lspctl.lua
@@ -0,0 +1,23 @@
+---@type LazySpec
+local spec = {
+  "clxmochamalefic/lspctl.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = { "Lspctl", },
+  keys = {
+    { "<space>lc", "<Cmd>Lspctl<CR>", desc = "lspctl" },
+  },
+  opt = {
+    info = "h",
+    start = "s",
+    stop = "x",
+    restart = "r",
+    close = "q",
+  },
+  config = function(_, opt)
+    require("lspctl").setup(opt)
+  end,
+}
+
+return spec
