d44a89412697334fcce779259bf73c286fa1b005
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-12-30 06:31:06 +0900

    add: neotest

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,24 @@
+---@type LazySpec
+local spec = {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-neotest/neotest-jest",
+    "marilari88/neotest-vitest",
+    "thenbe/neotest-playwright",
+  },
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        ["neotest-jest"] = {},
+        ["neotest-vitest"] = {},
+        ["neotest-playwright"] = {},
+      },
+    })
+  end
+}
+
+return spec
