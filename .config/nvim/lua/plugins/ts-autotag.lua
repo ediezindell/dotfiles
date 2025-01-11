f95e77f4cf609c7483e11334aa2a036cb066c00e
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2025-01-12 06:18:47 +0900

    refactor(nvim): add ts-autotag comment

--- .config/nvim/lua/plugins/ts-autotag.lua
+++ .config/nvim/lua/plugins/ts-autotag.lua
@@ -1,19 +1,21 @@
+--- HTML / JSXで自動でタグを閉じる
+--- NOTE: 動作しない場合は `:TSInstall` する
 ---@type LazySpec
 local spec = {
   "windwp/nvim-ts-autotag",
   opts = {
     opts = {
-      enable_close = false, -- Auto close tags
+      enable_close = true, -- Auto close tags
       enable_rename = true, -- Auto rename pairs of tags
       enable_close_on_slash = true, -- Auto close on trailing </
     },
     per_filetype = {
-      -- ["html"] = {
-      --   enable_close = false
-      -- }
+      -- html = {
+      --   enable_close = false,
+      -- },
     },
   },
-  event = { "BufReadPre", "BufNewFile" },
+  event = "LspAttach",
 }
 
 return spec
