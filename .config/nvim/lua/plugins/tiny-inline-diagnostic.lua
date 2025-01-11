4b2dfb9f2527a329e8655c2b6ab3fc625c435174
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2025-01-11 19:52:17 +0900

    feat(nvim): add tiny-inline-diagnostic.nvim

---
+++ .config/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,32 @@
+---@type LazySpec
+local spec = {
+  "rachartier/tiny-inline-diagnostic.nvim",
+  event = "LspAttach",
+  priority = 1000,
+  init = function()
+    vim.diagnostic.config({ virtual_text = false })
+  end,
+  opts = {
+    preset = "ghost",
+    options = {
+      multilines = {
+        enabled = true,
+        always_show = true,
+      },
+    },
+    signs = {
+      left = "",
+      right = "",
+      diag = "●",
+      arrow = "    ",
+      up_arrow = "    ",
+      vertical = " │",
+      vertical_end = " └",
+    },
+    blend = {
+      factor = 0.22,
+    },
+  },
+}
+
+return spec
