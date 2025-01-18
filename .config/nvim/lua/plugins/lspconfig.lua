dccbe6959aa8b2be3a080290be0956af12129ce2
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2025-01-19 08:17:10 +0900

    feat(nvim): TypeScriptのLS共存

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -23,8 +23,17 @@ local spec = {
     })
 
     lspconfig.denols.setup({
-      root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
-      -- single_file_support = true,
+      root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc", "denops", ".git"),
+      single_file_support = true,
+      autostart = false,
+      on_attach = function(_, buffer)
+        vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
+          buffer = buffer,
+          callback = function()
+            vim.cmd.DenolsCache()
+          end,
+        })
+      end,
       init_options = {
         lint = true,
         unstable = true,
@@ -34,14 +43,17 @@ local spec = {
               ["https://deno.land"] = true,
               ["https://cdn.nest.land"] = true,
               ["https://crux.land"] = true,
+              ["https://esm.sh"] = true,
             },
           },
         },
       },
       capabilities = capabilities,
     })
-    require("lspconfig.configs").vtsls = require("vtsls").lspconfig
-    lspconfig.vtsls.setup({})
+    lspconfig.vtsls.setup({
+      capabilities = capabilities,
+      autostart = false,
+    })
     lspconfig.lua_ls.setup(require("lsp.lua"))
     lspconfig.rust_analyzer.setup({})
     lspconfig.intelephense.setup(require("lsp.php"))
@@ -62,9 +74,6 @@ local spec = {
       },
       capabilities = capabilities,
     })
-    -- lspconfig.emmet_language_server.setup({
-    --   capabilities = capabilities,
-    -- })
     lspconfig.astro.setup({
       capabilities = capabilities,
     })
@@ -87,6 +96,30 @@ local spec = {
           command = "markuplint",
         }),
       },
+    })
+
+    -- TypeScriptのLS起動設定
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+      callback = function()
+        local configs = require("lspconfig.configs")
+
+        local ts_root = vim.fs.root(0, {
+          "tsconfig.json",
+          "jsconfig.json",
+        })
+        local deno_root = vim.fs.root(0, {
+          "deno.json",
+          "deno.jsonc",
+          "denops",
+        })
+
+        if ts_root ~= nil then
+          configs.vtsls.launch()
+        elseif deno_root ~= nil then
+          configs.denols.launch()
+        end
+      end,
     })
   end,
 }
