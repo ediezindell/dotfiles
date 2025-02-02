55b91cc13fe22d1490c971598eef3804070222b5
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2025-02-03 08:00:21 +0900

    feat(nvim): disable default schemes

--- .config/nvim/lua/plugins/denops-colorscheme.lua
+++ .config/nvim/lua/plugins/denops-colorscheme.lua
@@ -1,6 +1,9 @@
 ---@type LazySpec
 local spec = {
   "ediezindell/denops-colorscheme.vim",
+  init = function()
+    vim.g.denops_colorscheme_disable_default_scheme = 1
+  end,
   keys = {
     { "[c", "<Cmd>NextColor<CR>", desc = "next colorscheme" },
     { "]c", "<Cmd>PrevColor<CR>", desc = "prev colorscheme" },
