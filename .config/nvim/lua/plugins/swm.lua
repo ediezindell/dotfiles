28827f9874eccab50d0cfda02377291d6400e6dc
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2025-02-03 09:06:49 +0900

    refactor(nvim): swm

--- .config/nvim/lua/plugins/swm.lua
+++ .config/nvim/lua/plugins/swm.lua
@@ -2,13 +2,12 @@
 local spec = {
   "hrsh7th/nvim-swm",
   event = "BufEnter",
-  config = function()
-    local swm = require("swm")
-    vim.keymap.set({ "n", "x" }, "<C-h>", swm.h)
-    vim.keymap.set({ "n", "x" }, "<C-j>", swm.j)
-    vim.keymap.set({ "n", "x" }, "<C-k>", swm.k)
-    vim.keymap.set({ "n", "x" }, "<C-l>", swm.l)
-  end,
+  keys = {
+    { "<C-h>", '<Cmd>lua require("swm").h()<CR>' },
+    { "<C-j>", '<Cmd>lua require("swm").j()<CR>' },
+    { "<C-k>", '<Cmd>lua require("swm").k()<CR>' },
+    { "<C-l>", '<Cmd>lua require("swm").l()<CR>' },
+  },
 }
 
 return spec
