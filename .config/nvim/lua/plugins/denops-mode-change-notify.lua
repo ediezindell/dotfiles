6c4c48117b0b5395821419c5622fc9ddc9681420
ediezindell <ediezindell@gmail.com>
2025-02-05 06:58:47 +0900

    fix(nvim): remove autocmd

--- .config/nvim/lua/plugins/denops-mode-change-notify.lua
+++ .config/nvim/lua/plugins/denops-mode-change-notify.lua
@@ -5,31 +5,5 @@ local spec = {
     "vim-denops/denops.vim",
   },
   event = "BufEnter",
-  config = function()
-    vim.api.nvim_create_autocmd({
-      "ModeChanged",
-    }, {
-      pattern = "*:i",
-      callback = function()
-        vim.cmd([[call denops#request('denops-mode-change-notify', 'showToast', ["Insert"])]])
-      end,
-    })
-    vim.api.nvim_create_autocmd({
-      "ModeChanged",
-    }, {
-      pattern = "*:n",
-      callback = function()
-        vim.cmd([[call denops#request('denops-mode-change-notify', 'showToast', ["Normal"])]])
-      end,
-    })
-    vim.api.nvim_create_autocmd({
-      "ModeChanged",
-    }, {
-      pattern = "*:v",
-      callback = function()
-        vim.cmd([[call denops#request('denops-mode-change-notify', 'showToast', ["Visual"])]])
-      end,
-    })
-  end,
 }
 return spec
