fc33fc0d6e64fd67fe6d9a51de82a3ec15a2608a
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2025-02-25 12:53:20 +0900

    feat(nvim): barbarの表示設定やkeymapを追加

--- .config/nvim/lua/plugins/barbar.lua
+++ .config/nvim/lua/plugins/barbar.lua
@@ -3,19 +3,27 @@
 local spec = {
   "romgrk/barbar.nvim",
   dependencies = {
-    "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+    "lewis6991/gitsigns.nvim",     -- OPTIONAL: for git status
     "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
   },
   event = "BufEnter",
   keys = {
-    { "[b", "<Cmd>BufferPrevious<CR>", desc = "BufferPrevious (barbar)" },
-    { "]b", "<Cmd>BufferNext<CR>", desc = "BufferNext (barbar)" },
-    { "<A-w>", "<Cmd>BufferClose<CR>", desc = "BufferClose (barbar)" },
+    { "[b",        "<Cmd>BufferPrevious<CR>",                   desc = "BufferPrevious (barbar)" },
+    { "]b",        "<Cmd>BufferNext<CR>",                       desc = "BufferNext (barbar)" },
+    { "<A-w>",     "<Cmd>BufferClose<CR>",                      desc = "BufferClose (barbar)" },
+    { "<space>bp", "<Cmd>BufferPin<CR>",                        desc = "BufferPin (barbar)" },
+    { "<space>bD", "<Cmd>BufferCloseAllButCurrentOrPinned<CR>", desc = "BufferCloseAllButCurrentOrPinned (barbar)" },
   },
   init = function()
     vim.g.barbar_auto_setup = false
   end,
-  opts = {},
+  opts = {
+    highlight_alternate = true,
+    icons = {
+      separator = { left = "" },
+      pinned = { button = "", filename = true },
+    },
+  },
 }
 
 return spec
