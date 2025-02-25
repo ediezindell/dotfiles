2b55f2fbe9006133b75c62a6185563fab05ee288
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2025-02-25 09:45:31 +0900

    fix(nvim): settings of undo-glow.nvim

--- .config/nvim/lua/plugins/undo-glow.lua
+++ .config/nvim/lua/plugins/undo-glow.lua
@@ -3,32 +3,18 @@ local spec = {
   "y3owk1n/undo-glow.nvim",
   event = "BufEnter",
   opts = {
-    duration = 1000,
+    animation = {
+      enabled = true,
+      duration = 500,
+    },
   },
-  config = function()
-    -- Also add `BufReadPost` so that it will also highlight for first changes
-    vim.api.nvim_create_autocmd({ "BufReadPost", "TextChanged" }, {
-      pattern = "*",
-      callback = function()
-        -- Either provide a list of ignored filetypes
-        local ignored_filetypes = { "mason", "snacks_picker_list", "lazy", "oil" }
-        if vim.tbl_contains(ignored_filetypes, vim.bo.filetype) then
-          return
-        end
-
-        -- or just use buftype to ignore all other type
-        if vim.bo.buftype ~= "" then
-          return
-        end
-
-        -- then run undo-glow with your desired hlgroup
-        vim.schedule(function()
-          require("undo-glow").attach_and_run({
-            hlgroup = "UgUndo",
-          })
-        end)
-      end,
-    })
+  config = function(_, opts)
+    NormalKeymap("u", function()
+      require("undo-glow").undo(opts)
+    end, "undo")
+    NormalKeymap("<C-r>", function()
+      require("undo-glow").redo(opts)
+    end, "redo")
   end,
 }
 
