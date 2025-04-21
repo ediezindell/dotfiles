6d0dc63a4322d8570a25a22a28fd73c21573591e
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2025-04-22 08:40:13 +0900

    chore(nvim): update copilot keymaps

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -3,7 +3,22 @@ local spec = {
   enabled = true,
   "CopilotC-Nvim/CopilotChat.nvim",
   dependencies = {
-    { "github/copilot.vim" },
+    {
+      "github/copilot.vim",
+      init = function()
+        vim.g.copilot_no_tab_map = true
+      end,
+      keys = {
+        {
+          "<C-e>",
+          'copilot#Accept("")',
+          mode = "i",
+          expr = true,
+          replace_keycodes = false,
+          desc = "Accept Copilot suggestion",
+        },
+      },
+    },
     { "nvim-lua/plenary.nvim", branch = "master" },
   },
   build = "make tiktoken",
