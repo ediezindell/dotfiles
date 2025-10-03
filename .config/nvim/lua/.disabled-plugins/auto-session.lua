084d286a0f39dff7e1d57b7e05331ebb478ae905
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2025-10-03 10:30:38 +0900

    chore(nvim): disable some plugins

--- .config/nvim/lua/.disabled-plugins/auto-session.lua
+++ .config/nvim/lua/.disabled-plugins/auto-session.lua
@@ -4,8 +4,8 @@ local spec = {
   lazy = false,
   keys = {
     -- Will use Telescope if installed or a vim.ui.select picker otherwise
-    { "<leader>wr", "<cmd>SessionSearch<CR>", desc = "Session search" },
-    { "<leader>ws", "<cmd>SessionSave<CR>", desc = "Save session" },
+    { "<leader>wr", "<cmd>SessionSearch<CR>",         desc = "Session search" },
+    { "<leader>ws", "<cmd>SessionSave<CR>",           desc = "Save session" },
     { "<leader>wa", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
   },
 
